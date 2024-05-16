b7e3d937909b5e55608a9eb59f92266e8672214a
justinp04 <justin.pan688@gmail.com>
2024-05-17 07:31:11 +0800

    P07 work

--- P06/express.js
+++ P06/express.js
@@ -1,10 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const Joi = require('@hapi/joi')
 const app = express();
-
-app.get('/', function(req, res)
-{
-    res.send('Hello World!!',);
-})
 
 // req = request
 // res = response
@@ -14,6 +11,8 @@ const posts = [
     {id: 2, name:'Second post'},
     {id: 3, name:'Third post'}
 ];
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/posts', (req, res) => {
     res.send(posts)
@@ -28,9 +27,39 @@ app.get('/post/:postID', (req, res) => {
 });
 
 app.get('/post/:year/:month', (req, res) => {
-    res.send(req.params)
+    res.send(req.query);
 });
 
+app.post('/', function(req, res) {
+    console.log('Got a post request');
+    res.send('Post req received');
+});
+
+app.put('/posts/:id', function(req, res) {
+    console.log('Got a put request');
+    res.send('Put req received');
+});
+
+app.delete('/', function(req, res) {
+    console.log('Got a delete request');
+    res.send('Delete req received');
+});
+
+app.get('/', function(req, res)
+{
+    // Load the HTML file
+    res.sendFile(__dirname + '/index.html');
+})
+
+// The send request
+app.post('/send', function(req, res) {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+// The following line of code will allow me to load the files that are in the directory stated
+// app.use(express.static('../P06'));
+
 app.listen(3000, function() {
-    console.log('Listening on port 3000')
+    console.log('Listening on port 3000');
 });
