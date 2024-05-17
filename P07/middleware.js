df2e98996fa7024773759aab121c891c10d16420
justinp04 <justin.pan688@gmail.com>
2024-05-17 09:58:36 +0800

    Continued P07

---
+++ P07/middleware.js
@@ -0,0 +1,20 @@
+const express = require( 'express' ); 
+const app = express(); 
+
+const helmet = require( 'helmet' ); 
+const morgan = require( 'morgan' ); 
+const timeout = require( 'connect-timeout' ); 
+
+// Pug is a template engine
+app.set('view engine', 'pug');
+
+app.use(helmet()); 
+app.use(morgan( 'tiny' )); 
+app.use(timeout( '5s' )); 
+
+// An empty endpoint to try things out. This won t respond! 
+app.get( '/' , (req, res) => {
+    res.render('index', {theTitle: 'COMP6006 app', theMessage: 'Hello'});
+}); 
+
+app.listen(3000, () => {console.log("Listening on 3000.")});
